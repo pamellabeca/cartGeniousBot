3cf6dcf7ef5f09d3d3da33a387a7cb0ff558e001
pamellabeca <pamellasilva296@gmail.com>
2025-04-16 12:58:40 -0300

    feat: routes to list all categories and get information of them

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ItemController = require('../controllers/Item/ItemController');
+const CategoryController = require('../controllers/Category/CategoryController');
+
 
 router.post("/add", ItemController.addToWishlist);
 router.put("/addCart", ItemController.addToCart);
 router.delete("/remove", ItemController.removeItem);
+
+router.get("/categories", CategoryController.listAll);
+router.get('/categories/:name', CategoryController.categoryInformations);
 
 module.exports = router;
